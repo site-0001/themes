9bc721730eb32fca1e22a88faf8da720eb72980f
Kitsune Solar <kitsune.solar@gmail.com>
2021-04-04 23:38:25 +0300

    2021-04-04 20:38:25

--- ui/assets/js/theme.js
+++ ui/assets/js/theme.js
@@ -4,13 +4,14 @@
 // Init.
 // ---------------------------------------------------------------------------------------------------------------------
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   extGallery();
   extSelectAll();
   extPopUp();
   extGoogleForm();
   extMap();
   extDiagram();
+  extTabs();
 });
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -151,4 +152,25 @@ function extDiagram() {
   };
 
   mermaid.initialize($config);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// BS Current tab.
+// ---------------------------------------------------------------------------------------------------------------------
+
+function extTabs() {
+  const $elID = $('ul.node-tabs').attr('id');
+  const $elTab = $('a[data-bs-toggle="tab"]');
+  const $storageItem = $elID + '-active-tab';
+
+  $elTab.on('show.bs.tab', function (e) {
+    localStorage.setItem($storageItem, $(e.target).attr('href'));
+  });
+
+  const $activeTab = localStorage.getItem($storageItem);
+
+  if ($activeTab) {
+    const $id = $('#' + $elID)
+    $id.find('a[href="' + $activeTab + '"]').tab('show');
+  }
 }
