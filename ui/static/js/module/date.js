1aeb66424f05be75a4d49c5ff9ba9ad2cb54b324
Kitsune Solar <kitsune.solar@gmail.com>
2021-04-17 02:23:31 +0300

    2021-04-16 23:23:31

---
+++ ui/static/js/module/date.js
@@ -0,0 +1,50 @@
+export function local() {
+  const $el = queryAll('.ext-date-local');
+  let $utcDate, $localDate;
+
+  Array.from($el).forEach($i => {
+    $utcDate = attr($i, 'datetime');
+    $localDate = date($utcDate);
+    $i.textContent = $localDate;
+  });
+}
+
+function date($iso, $type = 0) {
+  let $date, $year, $month, $day, $hours, $minutes, $seconds, $out;
+
+  $date = new Date($iso);
+  $year = $date.getFullYear();
+  $month = f24($date.getMonth() + 1);
+  $day = f24($date.getDate());
+  $hours = f24($date.getHours());
+  $minutes = f24($date.getMinutes());
+  $seconds = f24($date.getSeconds());
+
+  function f24($num) {
+    if ($num < 10) {
+      $num = '0' + $num;
+    }
+    return $num;
+  }
+
+  switch ($type) {
+    case 0:
+      $out = $year + '/' + $month + '/' + $day + ' ' + $hours + ':' + $minutes + ':' + $seconds;
+      break;
+    case 1:
+      $out = $year + '/' + $month + '/' + $day;
+      break;
+    default:
+      console.log('Unknown type!')
+  }
+
+  return $out;
+}
+
+function queryAll($element) {
+  return document.querySelectorAll($element);
+}
+
+function attr($i, $attr) {
+  return $i.getAttribute($attr);
+}
--- ui/static/js/theme.js
+++ ui/static/js/theme.js
@@ -3,9 +3,14 @@ import * as share from './module/share.min.js';
 import * as select from './module/select.min.js';
 import * as gallery from './module/gallery.min.js';
 import * as konami from './module/konami.min.js';
+import * as date from './module/date.min.js';
 
+// Internal.
 bs.nodeActiveTab();
 share.popUp();
 select.all();
+date.local();
+
+// External.
 gallery.init();
 konami.init();
